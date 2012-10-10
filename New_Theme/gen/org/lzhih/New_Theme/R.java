12dfa95748bb5243294a9532103b7c96f313cbff
Lenovo <Lenovo@192.168.0.3>
2012-10-10 10:56:51 +0800

    加了几个新的！

--- New_Theme/gen/org/lzhih/New_Theme/R.java
+++ New_Theme/gen/org/lzhih/New_Theme/R.java
@@ -24,27 +24,33 @@ public final class R {
         public static final int e=0x7f02000a;
         public static final int ic_launcher=0x7f02000b;
         public static final int imagebutton=0x7f02000c;
-        public static final int progress=0x7f02000d;
-        public static final int progress_horizontal=0x7f02000e;
-        public static final int progress_medium_white=0x7f02000f;
-        public static final int radio=0x7f020010;
-        public static final int radio_1=0x7f020011;
-        public static final int radiobutton=0x7f020012;
-        public static final int rating=0x7f020013;
-        public static final int spinner=0x7f020014;
-        public static final int spinner_white_48=0x7f020015;
+        public static final int imagebutton_start=0x7f02000d;
+        public static final int progress=0x7f02000e;
+        public static final int progress_horizontal=0x7f02000f;
+        public static final int progress_medium_white=0x7f020010;
+        public static final int radio=0x7f020011;
+        public static final int radio_1=0x7f020012;
+        public static final int radiobutton=0x7f020013;
+        public static final int rating=0x7f020014;
+        public static final int seek_thumb=0x7f020015;
+        public static final int seek_thumb_pressed=0x7f020016;
+        public static final int spinner=0x7f020017;
+        public static final int spinner_white_48=0x7f020018;
     }
     public static final class id {
         public static final int button1=0x7f060001;
         public static final int button2=0x7f060000;
         public static final int button3=0x7f060002;
         public static final int checkBox=0x7f060003;
-        public static final int progressBar2=0x7f060006;
-        public static final int radio0=0x7f060008;
-        public static final int radio1=0x7f060009;
-        public static final int radio2=0x7f06000a;
-        public static final int radioGroup1=0x7f060007;
-        public static final int ratingBar1=0x7f060005;
+        public static final int imageButton1=0x7f06000b;
+        public static final int progressBar2=0x7f060005;
+        public static final int radio0=0x7f060007;
+        public static final int radio1=0x7f060008;
+        public static final int radio2=0x7f060009;
+        public static final int radioGroup1=0x7f060006;
+        public static final int scrollView1=0x7f06000d;
+        public static final int scrollView2=0x7f06000a;
+        public static final int seekBar1=0x7f06000c;
         public static final int spinner=0x7f060004;
     }
     public static final class layout {
@@ -55,9 +61,11 @@ public final class R {
         public static final int hello=0x7f040000;
     }
     public static final class style {
+        public static final int  SeekBar=0x7f05000b;
         public static final int App_Theme=0x7f050000;
         public static final int Button=0x7f050001;
         public static final int CheckBox=0x7f050002;
+        public static final int ImageButton=0x7f05000a;
         public static final int ProgressBar=0x7f050009;
         public static final int RadioButton=0x7f050008;
         public static final int RatingBar=0x7f050005;
